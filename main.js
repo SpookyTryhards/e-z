464a6cd10b750522dc8b386af2c1be8a1c035849
SpookyTryhards <94879186+SpookyTryhards@users.noreply.github.com>
2021-11-30 22:12:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' ) 
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+    
+   
+}
+console.log('ml5 version:', ml5.version)
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_s195BzE/model.json',modelLoaded); 
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "thumbs_up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "peace_sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "thumbs_up")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "peace_sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
